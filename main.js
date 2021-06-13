b8924b0dec3db53c4b5a7426a621e542c641b5f7
Viktor Bem <malchikk@live.com>
2021-06-13 15:08:30 +0200

    refactor main.js to make it more readable

--- main.js
+++ main.js
@@ -1,69 +1,60 @@
-class Table {
+const table = document.getElementById("tab");
+
+
+class Grid {
+    constructor() {
+        this.showTable(this.inputSudoku());
+    }
 
     readSudoku() {
         var sudoku = [];
-        for (var r=0; r<9; r++) {
+        for (var r = 0; r < 9; r++) {
             sudoku.push([]);
-            for (var c=0; c<9; c++) {
+            for (var c = 0; c < 9; c++) {
                 var id = r.toString() + c.toString();
                 var value = document.getElementById(id).value;
                 if (value === "") {
                     sudoku[r].push(0);
                 } else {
                     sudoku[r].push(parseInt(value));
-                }                    
+                }
             }
         }
         return sudoku;
     }
 
-    solve() {
-        var board = sudoku.readSudoku();
-        var solver = new Solver(board);
-        sudoku.showOutputTable(solver.solve());
+    inputSudoku() {
+        var sudoku = [];
+        for (var r = 0; r < 9; r++) {
+            sudoku.push([]);
+            for (var c = 0; c < 9; c++) {
+                var id = r.toString() + c.toString();
+                var inputField = document.createElement("input");
+                inputField.setAttribute("type", "text");
+                inputField.setAttribute("id", id);
+                sudoku[r].push(inputField);
+            }
+        }
+        return sudoku;
     }
 
-    showInputTable() {
-        var table = "";
-        for (var r=0; r<9; r++) {
-            if (r%3 === 0) {
-                table += "<tr class='topDiv'>";
-            } else {
-                table += "<tr>";
+    showTable(board) {
+        table.innerHTML = "";
+        for (var r = 0; r < 9; r++) {
+            var row = document.createElement("tr");
+            if (r % 3 === 0) {
+                row.setAttribute("class", "topDiv")
             }
-            for (var c=0; c<9; c++) {
-                if (c%3 === 0) {
-                    table += "<td class='leftDiv'>";
-                } else {
-                    table += "<td>";
+            for (var c = 0; c < 9; c++) {
+                var col = document.createElement("td");
+                if (c % 3 === 0) {
+                    col.setAttribute("class", "leftDiv");
                 }
-                table += "<input type='text' id='" + r + c+ "'>" + "</td>";
+                col.appendChild(board[r][c]);
+                row.appendChild(col);
             }
-            table += "</tr>";
+            table.appendChild(row);
         }
-        document.getElementById("tab").innerHTML = table;
-    }
-
-    showOutputTable(board) {
-        var table = "";
-        for (var r=0; r<9; r++) {
-            if (r%3 === 0) {
-                table += "<tr class='topDiv'>";
-            } else {
-                table += "<tr>";
-            }
-            for (var c=0; c<9; c++) {
-                if (c%3 === 0) {
-                    table += "<td class='leftDiv'>";
-                } else {
-                    table += "<td>";
-                }
-                table += board[r][c] + "</td>";
-
-            }
-            table += "</tr>";
-        }
-        document.getElementById("tab").innerHTML = table;
     }
 }
 
@@ -76,7 +67,19 @@ class Solver {
     solve() {
         var filled = this.fill();
         if (filled === true) {
-            return this.sudoku;
+            var solved = [];
+            for (var r = 0; r < 9; r++) {
+                solved.push([]);
+                for (var c = 0; c < 9; c++) {
+                    var cell = document.createElement("span");
+                    cell.innerHTML = this.sudoku[r][c];
+                    solved[r].push(cell);
+                }
+            }
+            return solved;
+        } else {
+            alert("This sudoku has no solution. Please check your entries.");
+            return null;
         }
     }
 
@@ -85,13 +88,13 @@ class Solver {
         if (coord === -1) {
             return true;
         }
-        var r = (Math.floor(coord/9));
-        var c = coord%9;
+        var r = (Math.floor(coord / 9));
+        var c = coord % 9;
 
-        for (var number=1; number<10; number++) {
+        for (var number = 1; number < 10; number++) {
             if (this.isValid(number, r, c)) {
                 this.sudoku[r][c] = number;
-                if (this.solve()) {
+                if (this.fill()) {
                     return true;
                 }
                 this.sudoku[r][c] = 0;
@@ -101,10 +104,10 @@ class Solver {
     }
 
     nextEmpty() {
-        for (var r=0; r<9; r++) {
-            for (var c=0; c<9; c++) {
+        for (var r = 0; r < 9; r++) {
+            for (var c = 0; c < 9; c++) {
                 if (this.sudoku[r][c] === 0) {
-                    return 9*r+c;
+                    return 9 * r + c;
                 }
             }
         }
@@ -115,16 +118,16 @@ class Solver {
         if (this.sudoku[r].includes(number)) {
             return false;
         }
-        for (var row=0; row<9; row++) {
+        for (var row = 0; row < 9; row++) {
             if (this.sudoku[row][c] === number) {
                 return false;
             }
         }
-        var boxRow = (Math.floor(r/3)) * 3;
-        var boxCol = (Math.floor(c/3)) * 3;
-        for (var row=0; row<3; row++) {
-            for (var col=0; col<3; col++) {
-                if (this.sudoku[row+boxRow][col+boxCol] === number) {
+        var boxRow = (Math.floor(r / 3)) * 3;
+        var boxCol = (Math.floor(c / 3)) * 3;
+        for (var row = 0; row < 3; row++) {
+            for (var col = 0; col < 3; col++) {
+                if (this.sudoku[row + boxRow][col + boxCol] === number) {
                     return false;
                 }
             }
@@ -134,11 +137,17 @@ class Solver {
 }
 
 
-var sudoku = new Table();
-sudoku.showInputTable();
+function solve() {
+    var board = grid.readSudoku();
+    var solved = new Solver(board).solve();
+    if (solved != null) {
+        grid.showTable(solved);
+    }
+}
 
-var solveBTN = document.getElementById("solve");
-var resetBTN = document.getElementById("reset");
+function reset() {
+    grid = new Grid();
+}
 
-solveBTN.addEventListener("click", sudoku.solve);
-resetBTN.addEventListener("click", sudoku.showInputTable);
+
+var grid = new Grid();
